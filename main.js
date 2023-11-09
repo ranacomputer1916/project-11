fd9e80651b0e8ce562b39f3531b08f004ecb2d1c
ranacomputer1916 <136680289+ranacomputer1916@users.noreply.github.com>
2023-11-09 21:06:58 +0600

    nice project

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// Typed Js Start
+var typed = new Typed('#typed', {
+  strings: ['Web Developer', 'Web Desigener', 'Full Stack Deveoper'],
+  typeSpeed: 50,
+  loop: true,
+  loopCount: Infinity,
+});
+// Typed Js End
+
+// Aos Start
+AOS.init();
